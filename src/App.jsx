3a081d7e114e602376c5ee794456e31bfe7a9f67
Arkad0 <danielcardenasparra@gmail.com>
2022-06-22 15:57:28 -0500

    control 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { MoviesGrid } from "./MoviesGrid";
+import { MoviesGrid } from "./components/MoviesGrid";
 import styles from "./App.module.css";
 
-export function App(){
-    return <div>
-        <header>
-            <h1 className={styles.title}>Movies</h1>
-        </header>
-        <main>
-           <MoviesGrid/>
-        </main>
+// import { MovieDetails } from "./pages/MovieDetails";
+// import { LandingPage,  } from "./pages/LandingPage";
+
+export function App() {
+  return (
+    <div>
+      <header>
+        
+          <h1 className={styles.title}>Movies</h1>
+        
+      </header>
+      <main>
+        <MoviesGrid/>
+      </main>
     </div>
+  );
 }
